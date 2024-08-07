8d65d7c7134548e1a3700cac2dc864e7af54b981
Pablo Souza <pablo.osouza@outlook.com>
2024-08-07 09:38:06 -0300

    feat: result pattern

---
+++ src/CleanSheet.Domain/Abstractions/Error.cs
@@ -0,0 +1,4 @@
+﻿namespace CleanSheet.Domain.Abstractions;
+public sealed record Error(
+    string Code,
+    string Message);
---
+++ src/CleanSheet.Domain/Abstractions/Result.cs
@@ -0,0 +1,28 @@
+﻿namespace CleanSheet.Domain.Abstractions;
+public record Result
+{
+    protected internal Result(bool isSuccess, Error? error = default)
+    {
+        IsSuccess = isSuccess;
+        Error = error;
+    }
+
+    public bool IsSuccess { get; }
+    public Error? Error { get; }
+
+    public static Result Success() => new(true, null);
+    public static Result Failure(Error error) => new(false, error);
+    public static Result<TValue> Success<TValue>(TValue value) => new(true, value: value);
+    public static Result<TValue> Failure<TValue>(Error error) => new(false, error);
+}
+
+public record Result<TValue> : Result
+{
+    protected internal Result(bool isSuccess, Error? error = default, TValue? value = default)
+        : base(isSuccess, error)
+    {
+        Value = value;
+    }
+
+    public TValue? Value { get; }
+}
